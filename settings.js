b037003544b51ae5e202b0991c4b0adf0f01927f
LordKagesian <lordkagesian@gmail.com>
2026-01-19 08:57:44 +0530

    Update bot settings for Arlecchino

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Knight Bot',
+  packname: 'Arlecchino',
   author: '‎',
-  botName: "Knight Bot",
+  botName: "Arlecchino",
   botOwner: 'Professor', // Your name
-  ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
+  ownerNumber: '639506472158', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   maxStoreMessages: 20, 
